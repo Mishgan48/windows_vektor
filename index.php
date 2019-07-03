4f5105f2731066ef7dfd22298bf5e9913237df25
Mike <mike333schum@gmail.com>
2019-07-03 16:59:21 +0300

    structure + head

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <?php
+      require('require/head.php');
+    ?>
+  </head>
+
+  <body>
+    <?php
+      require('require/header.php');
+    ?>
+    
+    <?php
+      require('require/footer.php');
+    ?>
+  </body>
+</html>
---
+++ require/head.php
@@ -0,0 +1,13 @@
+<meta charset="utf-8">
+<title>Оконный вектор</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="">
+<meta name="description" content="">
+
+<link rel="icon" type="image/png" href="img/favicon.png">
+<link rel="stylesheet" href="font/akrobat/akrobat.css" />
+<link rel="stylesheet" href="style/style.css?ver=1" />
+
+<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="js/jquery.maskedinput.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
