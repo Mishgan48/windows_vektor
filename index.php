22c4731a12802ffe829220761884e7f947e15300
Mike <mike333schum@gmail.com>
2019-07-03 18:56:14 +0300

    half top

--- index.php
+++ index.php
@@ -7,10 +7,29 @@
   </head>
 
   <body>
-    <?php
-      require('require/header.php');
-    ?>
-    
+    <header>
+      <?php
+        require('require/header.php');
+      ?>
+      <div class="header__top">
+        <h1>оконный <span>вектор</span></h1>
+        <div>
+          <input type="text">
+          <img src="img/" alt="">
+          <p class="header__call"></p>
+        </div>
+      </div>
+      <nav>
+        <ul class="menu">
+          <li>окна</li>
+          <li>двери</li>
+          <li>дизайн окон</li>
+          <li>сервис услуги</li>
+          <li>о компании</li>
+        </ul>
+      </nav>
+    </header>
+
     <?php
       require('require/footer.php');
     ?>
