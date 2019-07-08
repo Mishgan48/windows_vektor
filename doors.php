c2a24ef5d32235b0aad8396d79609f7816e242b6
Mike <mike333schum@gmail.com>
2019-07-08 22:26:53 +0300

    doors

---
+++ doors.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <?php
+      require('require/head.php');
+    ?>
+  </head>
+
+  <body>
+    <header>
+      <?php
+        require('require/header.php');
+      ?>
+    </header>
+    <section class="content" id="doorcatalog">
+      <div class="doorcatalog__item layer1">
+        <p class="doorcatalog__item-title">Входные двери</p>
+        <p class="doorcatalog__item-text">Большой выбор пластиковых и деревянных входных дверей от ведущих мировых производителей сделают вашу жизнь комфортной и роскошной</p>
+        <!--<p class="doorcatalog__item-right">>>></p>-->
+      </div>
+      <div class="doorcatalog__item layer2">
+        <p class="doorcatalog__item-title">Межкомнатные двери</p>
+        <p class="doorcatalog__item-text">Большой выбор пластиковых и деревянных входных дверей от ведущих мировых производителей сделают вашу жизнь комфортной и роскошной</p>
+        <!--<p class="doorcatalog__item-right">>>></p>-->
+      </div>
+      <div class="doorcatalog__item layer3">
+        <p class="doorcatalog__item-title">Балконные двери</p>
+        <p class="doorcatalog__item-text">Большой выбор пластиковых и деревянных входных дверей от ведущих мировых производителей сделают вашу жизнь комфортной и роскошной</p>
+        <!--<p class="doorcatalog__item-right">>>></p>-->
+      </div>
+      <div class="doorcatalog__item layer4">
+        <p class="doorcatalog__item-title">Панорамные двери</p>
+        <p class="doorcatalog__item-text">Большой выбор пластиковых и деревянных входных дверей от ведущих мировых производителей сделают вашу жизнь комфортной и роскошной</p>
+        <!--<p class="doorcatalog__item-right">>>></p>-->
+      </div>
+    </section>
+    <footer>
+      <div class="content footer">
+        <?php
+          require('require/footer.php');
+        ?>
+      </div>
+    </footer>
+  </body>
+</html>
